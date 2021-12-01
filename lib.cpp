4bd6bcbfeaebbb7e3103daa43adfa04076eb1342
Rcerruti <91542393+Rcerruti@users.noreply.github.com>
2021-12-01 17:26:29 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
-
+#include "lib.h"
+using namespace std;
+bool funzione(char a, char &conversione){
+    bool risultato=true;
+    if ((a>=97 && a<=122)||(a>=65 && a<=90)){
+        risultato=true;
+        if(a<=122 && a>=97){
+            conversione=a-32;
+            return conversione;
+        }else if(a>=65 && a<=90){
+            conversione=a+32;
+            return conversione;
+        }
+    }else{
+        risultato=false;
+    }
+    return risultato;
+}
